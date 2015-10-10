2975f1eb43a66e9201f48bbd0b1fbdc8d1e886de
Alex Erygin <alex.erygin@gmail.com>
2015-10-10 23:46:28 +0300

    +sample project

---
+++ sources/Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var dialog = new SqlConnectionDialog.Dialog();
+			dialog.ShowDialog();
+		}
+	}
+}
--- sources/SqlConnectionDialog/Dialog.xaml.cs
+++ sources/SqlConnectionDialog/Dialog.xaml.cs
@@ -1,21 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SqlConnectionDialog
 {
 	/// <summary>
-	/// Interaction logic for Dialog.xaml
+	///     Interaction logic for Dialog.xaml
 	/// </summary>
 	public partial class Dialog : Window
 	{
 		public Dialog()
 		{
 			InitializeComponent();
-			
 		}
-
-		
 	}
-
-
 }
