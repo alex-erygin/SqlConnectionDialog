e22492211dcfe116168791f9b149dba09c57eaba
Alex Erygin <alex.erygin@gmail.com>
2015-10-10 23:11:07 +0300

    sql connection dialog

---
+++ sources/SqlConnectionDialog/Dialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace SqlConnectionDialog
+{
+	/// <summary>
+	/// Interaction logic for Dialog.xaml
+	/// </summary>
+	public partial class Dialog : Window
+	{
+		public Dialog()
+		{
+			InitializeComponent();
+			
+		}
+
+		
+	}
+
+
+}
