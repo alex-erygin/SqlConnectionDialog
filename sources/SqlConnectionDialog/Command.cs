7499a43a31f256cf87fc906f9c1e4e1f865f62a0
Alex Erygin <alex.erygin@gmail.com>
2015-10-11 12:27:31 +0300

    Tab indexes adjusted

---
+++ sources/SqlConnectionDialog/Command.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SqlConnectionDialog
+{
+	internal sealed class Command : ICommand
+	{
+		private readonly Predicate<object> canExecute;
+		private readonly Action<object> execute;
+
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		/// <param name="execute">Action to execute.</param>
+		public Command(Action<object> execute) : this(null, execute)
+		{
+		}
+
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		/// <param name="canExecute">Can execute.</param>
+		/// <param name="execute">Action to execute.</param>
+		public Command(Predicate<object> canExecute, Action<object> execute)
+		{
+			if (execute == null) throw new ArgumentNullException(nameof(execute));
+			this.canExecute = canExecute;
+			this.execute = execute;
+		}
+
+
+		/// <summary>
+		/// Defines the method to be called when the command is invoked.
+		/// </summary>
+		/// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+		public void Execute(object parameter)
+		{
+			if (canExecute != null && canExecute(parameter))
+			{
+				execute(parameter);
+				return;
+			}
+
+			execute(parameter);
+		}
+
+		/// <summary>
+		/// Defines the method that determines whether the command can execute in its current state.
+		/// </summary>
+		/// <returns>
+		/// true if this command can be executed; otherwise, false.
+		/// </returns>
+		/// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+		public bool CanExecute(object parameter)
+		{
+			if (canExecute != null)
+			{
+				return canExecute(parameter);
+			};
+
+			return true;
+		}
+
+		public event EventHandler CanExecuteChanged;
+	}
+
+
+}
--- sources/SqlConnectionDialog/Dialog.xaml.cs
+++ sources/SqlConnectionDialog/Dialog.xaml.cs
@@ -1,15 +1,67 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace SqlConnectionDialog
 {
 	/// <summary>
-	///     Interaction logic for Dialog.xaml
+	///     Sql connection window
 	/// </summary>
 	public partial class Dialog : Window
 	{
+		/// <summary>
+		/// Ctor.
+		/// </summary>
 		public Dialog()
 		{
 			InitializeComponent();
+			SetupFocus();
+			InitDataContext();
+			InitCommands();
+		}
+
+		public string ServerName { get; set; }
+
+		public string DatabaseName { get; set; }
+
+		public string Authentication { get; set; }
+
+		public string UserName { get; set; }
+
+		
+		public ICommand TestCommand { get; set; }
+		
+		public ICommand CancelCommand { get; set; }
+		
+		public ICommand OkCommand { get; set; }
+
+
+		private void SetupFocus()
+		{
+			
+		}
+
+		private void InitCommands()
+		{
+		}
+
+		private void InitDataContext()
+		{
+			DataContext = this;
+		}
+
+		private void Ok()
+		{
+			
+		}
+
+		private void Cancel()
+		{
+			
+		}
+
+		private void Test()
+		{
+			
 		}
 	}
 }
