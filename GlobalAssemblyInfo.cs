2d41cf01f58d6422a58507bebdff6e5bf904a1f2
Alex Erygin <alex.erygin@gmail.com>
2015-10-12 22:04:21 +0300

    use global assembly info

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿[assembly: System.Reflection.AssemblyProduct("Connection string factory")]
+[assembly: System.Reflection.AssemblyCompany("Aleksandr Erygin")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright 2015 Aleksandr Erygin.")]
+[assembly: System.Reflection.AssemblyTrademark("Connection string factory")]
+[assembly: System.Reflection.AssemblyConfiguration("Release")]
+[assembly: System.Reflection.AssemblyVersion("0.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("Compiled By alex")]
